a407cb6708ac95bf50e0be88b3c22f6a39ecb7fe
Oksana Kuzich <komoks255@gmail.com>
2024-11-18 14:51:41 +0100

    Initial commit

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,37 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            env("S3_BUCKET") + "." + env("S3_ENDPOINT"),
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            env("S3_BUCKET") + "." + env("S3_ENDPOINT"),
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
---
+++ src/utiles/theme.js
@@ -0,0 +1,29 @@
+export const theme = {
+  colors: {
+    main: "#F4F4F4",
+    accent: "#242424",
+    stars: "#EEA10C",
+    white: "#ffffff",
+    background: "#040404",
+    footer: "#C6CDD1",
+    pagination: "#D4D4D4",
+  },
+  fonts: {
+    // main: "Roboto, sans-serif",
+    // secondary: "Open Sans, sans-serif",
+    // accent: "Yellowtail, sans-serif",
+  },
+  fontSizes: {
+    fs12: "12px",
+    fs14: "14px",
+    fs16: "16px",
+    fs18: "18px",
+    fs24: "24px",
+    fs44: "44px",
+    fs74: "74px",
+  },
+  fontWeights: {
+    fw400: "400",
+    fw500: "500",
+  },
+};
